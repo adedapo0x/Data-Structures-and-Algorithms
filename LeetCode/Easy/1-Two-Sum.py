80a7e51c9626e6fe891927fa59562dc14c23f48b
Adeyemi Adedapo <adedapo025@gmail.com>
2024-09-11 23:34:54 +0100

    Noted solution was not optimized

--- LeetCode/Easy/1-Two-Sum.py
+++ LeetCode/Easy/1-Two-Sum.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums, target: int): 
-
-               
+        # not so optimized solution (two pass hash table)    
         hash = {x:nums.index(x) for x in nums}
         for i in range(len(nums)):
             secondVal = target - nums[i]
