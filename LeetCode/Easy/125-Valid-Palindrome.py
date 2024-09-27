fff866fb821a59bfd7628ecab4c8de047d7ff4b1
Adeyemi Adedapo <adedapo025@gmail.com>
2024-09-27 20:02:46 +0100

    another soln for valid palindrome added

--- LeetCode/Easy/125-Valid-Palindrome.py
+++ LeetCode/Easy/125-Valid-Palindrome.py
@@ -1,15 +1,19 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-
-
+        # loops, then uses built in alphanum() method to check string before reversing and checking
+        newStr = ""
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
 
 
         # personal first solution, using re module  to check for alphanumeric and using split, join
-        import re
-        arr = re.split('[^a-z0-9]', s.lower()) 
-        newStr = ''.join(arr)
-        return newStr[0::] == newStr[::-1]
+        # import re
+        # arr = re.split('[^a-z0-9]', s.lower()) 
+        # newStr = ''.join(arr)
+        # return newStr[0::] == newStr[::-1]
 
 
         
