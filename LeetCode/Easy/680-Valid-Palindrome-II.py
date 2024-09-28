93b40285a3b32e597f542609093bd8a60fc64dc2
Adeyemi Adedapo <adedapo025@gmail.com>
2024-09-28 09:47:42 +0100

    added recursive soln for validPalindrome II

--- LeetCode/Easy/680-Valid-Palindrome-II.py
+++ LeetCode/Easy/680-Valid-Palindrome-II.py
@@ -15,3 +15,16 @@ class Solution:
             l, r = l + 1, r - 1
         return True
     
+
+    # uses recursion, but changes input arguments
+
+# class Solution:
+#     def validPalindrome(self, s: str, check=False) -> bool:
+#         l, r = 0, len(s) - 1
+#         while l < r:
+#             if s[l] != s[r]:
+#                 if not check:
+#                     return self.validPalindrome(s[l:r], True) or self.validPalindrome(s[l+1:r+1], True)
+#                 return False
+#             l, r = l + 1, r - 1
+#         return True
