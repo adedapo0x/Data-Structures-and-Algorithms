9b9230954d47e9f3fae9f95861217594684bea9c
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-21 13:11:54 +0100

    added more optimal solution for valid parentheses

--- LeetCode/Easy/20-Valid-Parentheses.py
+++ LeetCode/Easy/20-Valid-Parentheses.py
@@ -2,27 +2,44 @@ from collections import deque
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        # using an helper function
-        if len(s) < 2:
-            return False
+        # first solution; without helper function
+
         stack = deque()
-        def matches(c1, c2):
-            obj = {
-                "(": ")",
-                "{": "}",
-                "[": "]"
-            }
-            return obj[c1] == c2
 
         for ch in s:
-            if ch == "(" or ch == "{" or ch == "[":
+            if ch in "({[":
                 stack.append(ch)
-            elif ch == ")" or ch == "}" or ch == "]":
-                if len(stack) == 0:
+            else:
+                if (len(stack) == 0
+                or (ch == ')' and stack[-1] != '(') 
+                or (ch == '}' and stack[-1] != '{') 
+                or (ch == ']' and stack[-1] != '[')):
                     return False
-                if not matches(stack.pop(), ch):
-                    return False
-        if len(stack) != 0:
-            return False
-        return True
+                stack.pop()
+        return not stack
+    
+
+        # using an helper function
+        # if len(s) < 2:
+        #     return False
+        # stack = deque()
+        # def matches(c1, c2):
+        #     obj = {
+        #         "(": ")",
+        #         "{": "}",
+        #         "[": "]"
+        #     }
+        #     return obj[c1] == c2
+         
+        # for ch in s:
+        #     if ch == "(" or ch == "{" or ch == "[":
+        #         stack.append(ch)
+        #     elif ch == ")" or ch == "}" or ch == "]":
+        #         if len(stack) == 0:
+        #             return False
+        #         if not matches(stack.pop(), ch):
+        #             return False
+        # if len(stack) != 0:
+        #     return False
+        # return True
                 
