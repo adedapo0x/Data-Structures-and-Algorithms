98f5db1da0156a148588aec5680946cf59a18814
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-21 15:28:25 +0100

    added reverse polish notation soln

---
+++ LeetCode/Medium/150-Evaluate-Reverse-Polish.py
@@ -0,0 +1,17 @@
+class Solution:
+    def evalRPN(self, tokens) -> int:
+        stack = []
+        for val in tokens:
+            if val == "+":
+                stack.append(stack.pop() + stack.pop())
+            elif val == "-":
+                a, b = stack.pop(), stack.pop()
+                stack.append(b - a)
+            elif val == "/":
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(b / a))
+            elif val == "*":
+                stack.append(stack.pop() * stack.pop())
+            else:
+                stack.append(int(val))
+        return stack[0]
