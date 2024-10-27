677d89f16b9efc8a6b256d030ded5da339863ba6
Adeyemi Adedapo <adedapo025@gmail.com>
2024-10-27 18:31:33 +0100

    added comments to encode and decode soln

--- LeetCode/Medium/659-Encode-And-Decode-Strings.py
+++ LeetCode/Medium/659-Encode-And-Decode-Strings.py
@@ -2,6 +2,8 @@ class Solution:
     def encode(self, strs) -> str:
         res = ""
         for s in strs:
+            # there is need for the hash symbol because the length might not be just one digit
+            # then we'll need something to tell where the length ends and the str begins
             res += str(len(s)) + "#" + s
         return res
 
@@ -11,11 +13,11 @@ class Solution:
         l, r = 0, 0
         while r < len(s):
             if s[r] == "#":
-                length = int(s[l:r])
-                l = r + 1
-                r += length + 1
+                length = int(s[l:r]) # gets length
+                l = r + 1 # puts left pointer at the beginning of the the word
+                r += length + 1 # increments left pointer to the index after the end of the word
                 res.append(s[l:r])
-                l = r
+                l = r # updates left character to begin process again for next word
             else:
                 r += 1
         return res
