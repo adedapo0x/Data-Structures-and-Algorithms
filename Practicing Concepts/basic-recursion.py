77b4f34952b2e4a84ca96928ed4a6f9c9a98d759
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-04 16:03:26 +0100

    added another soln for recursive reverse of an array

--- Practicing Concepts/basic-recursion.py
+++ Practicing Concepts/basic-recursion.py
@@ -7,3 +7,12 @@ def reverseRecursively(arr,i):
         return arr
     arr[i], arr[n-i-1] = arr[n-i-1], arr[i]
     return reverseRecursively(arr, i + 1)
+
+# another method to reverse array using recursion
+# two pointer approach
+
+def swap(arr, l, r):
+    if l >= r:
+        return arr
+    arr[l], arr[r] = arr[r], arr[l]
+    return swap(arr, l + 1, r - 1)
