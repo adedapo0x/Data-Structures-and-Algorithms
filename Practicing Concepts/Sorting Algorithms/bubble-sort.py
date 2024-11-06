5498bb8474e98caf8cac15c6b9280abd642fd58f
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-06 22:28:02 +0100

    added bubble sort algorithm definition

---
+++ Practicing Concepts/Sorting Algorithms/bubble-sort.py
@@ -0,0 +1,10 @@
+# In this algorithm, two loops are used. The outer loop starts from the end of the array and loops backwards 
+# up until just before the beginning, second loop counts from beginning of the array up until where the outer array counter is
+
+# This involves setting outer loop i at the end of the array, then inner loop counter j starts from the beginning of the array, 
+# checking if element at index j is greater than element after it (j+1), if so, it swaps them, this checking occurs up to where index i currently is
+# so this algorithm swaps bigger elements to the right and pushes smaller elements to the left in each pass
+
+#  
+
+ 
