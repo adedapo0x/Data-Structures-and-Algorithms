1c3d4553f0943fdfab53fde3a7fbf90b3e357e04
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-07 12:42:32 +0100

    added more info about bubble sort

--- Practicing Concepts/Sorting Algorithms/bubble-sort.py
+++ Practicing Concepts/Sorting Algorithms/bubble-sort.py
@@ -5,7 +5,13 @@
 # checking if element at index j is greater than element after it (j+1), if so, it swaps them, this checking occurs up to where index i currently is
 # so this algorithm swaps bigger elements to the right and pushes smaller elements to the left in each pass
 
-#  
+# Thus, after each iteration, the last part of the array will become sorted.
+# Like: after the first iteration, the array up to the last index will be sorted, 
+# and after the second iteration, the array up to the second last index will be sorted, and so on.
+
+# The check variable in the code is the optimization. check is initially set to 0, if any swap occurs it changes to 1,
+# after each iteration of outer loop, the check is set to 0 back, this is because once no swap occurs again and check remains 0
+# this means that the array is already sorted and there is no need to keep looping, hence the need for the condition to break out of the loop once this happens.  
 
 def bubbleSort(arr):
     n = len(arr)
@@ -18,3 +24,9 @@ def bubbleSort(arr):
         if check == 0:
             break
     return arr
+
+# Time complexity (worst case and average case):  O(n^2)
+# Time complexity (best case): O(n) ie if given an already sorted array
+
+# Space complexity: O(1)
+
