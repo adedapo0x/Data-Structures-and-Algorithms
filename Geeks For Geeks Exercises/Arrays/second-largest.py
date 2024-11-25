5e958821310185860c40f588e415c9362fc5b410
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-25 16:36:51 +0100

    added second Largest solution

---
+++ Geeks For Geeks Exercises/Arrays/second-largest.py
@@ -0,0 +1,70 @@
+# Given an array of positive integers arr[], return the second largest element from the array. 
+# If the second largest element doesn't exist then return -1.
+
+# Note: The second largest element should not be equal to the largest element.
+
+# Examples:
+
+# Input: arr[] = [12, 35, 1, 10, 34, 1]
+# Output: 34
+# Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+
+# Brute force solution
+# Time complexity: O(nlogn) + O(n) = O(nlogn)
+
+class Solution:
+    def getSecondLargest(self, arr):
+        arr.sort()
+        
+        largest = arr[-1]
+        
+        for i in range(len(arr) - 1, -1, -1):
+            if arr[i] != largest:
+                return arr[i]
+        return -1
+    
+
+# Better solution
+# Time complexity: O(n) + O(n) = O(2n) = O(n)
+
+class Solution:
+    def getSecondLargest(self, arr):
+        
+        largest = sLargest = 0
+        
+        # gets largest in the array
+        for n in arr:
+            if n > largest:
+                largest = n
+                
+        # to check for second largest
+        for n in arr:
+            if n > sLargest and n < largest:
+                sLargest = n
+                
+        if sLargest == 0:
+            return -1
+        else:
+            return sLargest
+        
+    
+# Optimal solution
+# Solution above is O(N) but takes two passes, there is better solution with just one pass
+# The optimal solution here takes O(N) in one pass
+
+class Solution:
+    def getSecondLargest(self, arr):
+        largest = sLargest = 0
+        
+        for n in arr:
+            if n > largest:
+                sLargest = largest
+                largest = n
+            elif n > sLargest and n != largest:
+                sLargest = n
+                
+        if sLargest == 0:
+            return -1 
+        else:
+            return sLargest
