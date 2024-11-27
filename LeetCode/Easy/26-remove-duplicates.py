7d7d94d347833bd2c3dff0ebd61c12b316a0dd2d
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-27 21:30:17 +0100

    added more optimal soln for remove duplicates

--- LeetCode/Easy/26-remove-duplicates.py
+++ LeetCode/Easy/26-remove-duplicates.py
@@ -1,6 +1,34 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
 
+        # optimal solution, shorter more direct code
+        # in this case loops never runs if the length of nums is 1
+        # TC: O(N), SC:O(1)
+
+        j = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[j] = nums[i]
+                j += 1
+        return j
+        
+
+
+        # better solution, still not completely optimal, uses two pointer method
+        # TC: O(N), SC: O(1)
+
+        k = 0
+        l = 0
+
+        for r in range(len(nums)):
+            if r != 0 and nums[r] == nums[r - 1]:
+                continue
+            k += 1
+            nums[l] = nums[r]
+            l += 1
+    
+        return k
+
 
         # not at all optimal, my first shot at the question, uses extra memory when the question said to solve it in-place
         # time complexity: O(N) but takes two passes
