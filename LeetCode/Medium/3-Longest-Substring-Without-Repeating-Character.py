7f2b6b565b95532a58b61d515be79e239ff067cc
Adeyemi Adedapo <adedapo025@gmail.com>
2024-11-28 12:39:46 +0100

    added more optimal soln for question 3

--- LeetCode/Medium/3-Longest-Substring-Without-Repeating-Character.py
+++ LeetCode/Medium/3-Longest-Substring-Without-Repeating-Character.py
@@ -1,27 +1,28 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # brute-force  solution:
-        # take every element of the string as a start of a sequence till the end and only break if duplicates are found
-        # we can check for duplicate in costant time using a set, once duplicate is found we break out of the inner loop and go to the next element in the string to use as the start of the sequence
 
-        # TC: O(N^2), SC: O(N)
+        # Most optimal solution
+        # This is also O(N) but here we can go directly to the next element after the one that is a duplicate to remove the duplicacy
+        # unlike the solution below where we have to move the window one by one and check if there is still a duplicate
 
-        longest = 0
-        for i in range(len(s)):
-            hashSet = set()
-            for j in range(i, len(s)):
-                if s[j] in hashSet:
-                    break
-                hashSet.add(s[j])
-            longest = max(longest, len(hashSet))
+        hashMap = {} # to ensure there is no duplicacy
+        l = longest = 0
+
+        for r in range(len(s)):
+            if s[r] in hashMap:
+                l = max(hashMap[s[r]] + 1, l) # max function is used here in case where the element initially occurs is no longer in the window we are considering
+            hashMap[s[r]] = r  # update hashMap if present before, or add new elements to the hash
+            longest = max(longest, r - l + 1)
         return longest
 
 
+        # Better solution 
         # Thought process:
         # Have a set to check for duplicates, keep left & right pointer at 0 
         # loop through the input str and check each time if current char is in set, if not add and increment r, since that is a substr with no duplicate
         # else, delete the char at the left from the substr, move left pointer to the right by 1, check again if duplicate still exists, again and again
        # we remove left pointer since the substr has to follow one another.
+       # TC: O(N), SC: O(N)
 
         char_set = set()
         l = 0
@@ -38,3 +39,21 @@ class Solution:
                 l += 1
         
         return max_length
+    
+
+
+        # brute-force  solution:
+        # take every element of the string as a start of a sequence till the end and only break if duplicates are found
+        # we can check for duplicate in costant time using a set, once duplicate is found we break out of the inner loop and go to the next element in the string to use as the start of the sequence
+
+        # TC: O(N^2), SC: O(N)
+
+        longest = 0
+        for i in range(len(s)):
+            hashSet = set()
+            for j in range(i, len(s)):
+                if s[j] in hashSet:
+                    break
+                hashSet.add(s[j])
+            longest = max(longest, len(hashSet))
+        return longest
