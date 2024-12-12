43a384a9d1b940b9a8cbc58283faf4cc3438edc1
Adeyemi Adedapo <adedapo025@gmail.com>
2024-12-12 22:47:53 +0100

    started learning linked lists

---
+++ Structy Practice/Linked List/linked-list-intro.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+# hardcoding a linked list for learning purposes
+
+a = Node("A")
+b = Node("B")
+c = Node("C")
+d = Node("D")
+
+a.next = b
+b.next = c
+c.next = d
+
+
+
