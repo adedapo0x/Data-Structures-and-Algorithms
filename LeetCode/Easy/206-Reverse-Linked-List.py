7a83c9a8ffbbcf42f5253ba99fbc13bb0ac3cbe0
Adeyemi Adedapo <adedapo025@gmail.com>
2024-12-14 20:59:49 +0100

    added reverse LL lc solution

---
+++ LeetCode/Easy/206-Reverse-Linked-List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # iterative method TC: O(1), O(N)
+        current = head
+        prev = None
+        while current:
+            nexxt = current.next
+            current.next = prev
+            prev = current
+            current = nexxt
+        return prev
+    
+
+       
+
+        
