3d81a3d0007d3c3ec5e2dd15f5f0a0ece344110d
Adeyemi Adedapo <adedapo025@gmail.com>
2024-12-22 02:45:32 +0100

    added optimal solns for LC 19

---
+++ LeetCode/Medium/19-Remove-Nth-Node-From-End.py
@@ -0,0 +1,50 @@
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # one pass method, creates dummy and uses two pointers O(N)
+        # logic could have been to keep the distance between left and right pointers by n, so when the right is None means left pointer is at the node we want to delete
+        # but this way we have no way of accessing previous node to change where it points to
+        # so we use a dummy node at the beginning, make distance be n + 1, that case when right reaches None, left is one node before the one we intend to delete
+        # so we just next it to the one after the to be deleted one
+        dummy = ListNode(0, head)
+        left = dummy
+        right = head
+
+        # gets right pointer to be n nodes away from head, the left being at dummy then makes distance n + 1
+        while n > 0 and right:
+            right = right.next
+            n -= 1
+
+        while right:  # moves the pointers till right becomes Null
+            left = left.next
+            right = right.next
+
+        left.next = left.next.next # actual redirection of node occurs
+        return dummy.next #returns head
+
+
+        # a two pass method of O(N) TC would be to reverse the LL, then delete from the front
+        # this is a two pass algorithm, can be done in one pass
+
+        # bruteforce solution(came up with this)
+        # stores nodes in array, then pass through once again skipping the nth one
+        # TC: O(N), SC: O(N)
+        # edge case at end of code...
+        nodes = []
+        curr = head
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
+
+        pos = len(nodes) - n
+        dummy = ListNode()
+        curr = dummy
+        for i in range(len(nodes)):
+            if i == pos:
+                continue
+            curr.next = nodes[i]
+            curr = curr.next
+
+        if n == 1: # if what to deleted is last node, make the node before it point to None unless it'll still 
+            curr.next = None  # point to it since it doesn't have anything after to otherwise point to
+        
+        return dummy.next
