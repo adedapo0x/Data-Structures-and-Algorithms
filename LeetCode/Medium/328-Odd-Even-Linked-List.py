5aa29e94e3c866657277b2f4f30b8f8bfccf2e1b
Adeyemi Adedapo <adedapo025@gmail.com>
2025-01-03 08:23:31 +0100

    added optimal solution for even-odd nodes

--- LeetCode/Medium/328-Odd-Even-Linked-List.py
+++ LeetCode/Medium/328-Odd-Even-Linked-List.py
@@ -1,5 +1,23 @@
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # optimal solution
+        # Uses two pointers, odd and even
+        # odd goes through connecting odd together, even does the same for even nodes
+        # we need a variable for even head in order to the link the tail of odd to after the loop is done
+        # we use even and even.next as the condition because of even is always after odd so no need checking for odd too
+        if not head or not head.next:
+            return head
+        odd, even = head, head.next
+        evenHead = head.next
+        while even and even.next:
+            odd.next = odd.next.next
+            even.next = even.next.next
+            odd = odd.next
+            even = even.next
+        odd.next = evenHead
+        return head
+
+
         # brute force approach, uses extra space to store nodes
         # TC: O(N), SC: O(N)
         if not head or not head.next:
