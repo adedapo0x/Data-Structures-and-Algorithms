e3e10db03fe0210cd14c537b51a7e3787105b8dd
Adeyemi Adedapo <adedapo025@gmail.com>
2025-01-04 18:44:59 +0100

    added optimization for LC 160

--- LeetCode/Easy/160-Intersection-Of-Two-LLs.py
+++ LeetCode/Easy/160-Intersection-Of-Two-LLs.py
@@ -1,9 +1,25 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # More optimal solution TC: O(N + M), SC: O(1)
+        # loops through both LL at the same time if they meet at a point in first iteration, loop exits and returns one of them, if there is no intersection for both, they'll both be None
+        # and None is returned. But if one is longer than the other by a distance d, when we do that if one reaches None first it switches to the second LL and start traversing when the second reaches None to it switches 
+        # to first and starts traversing. they end up being on same wavelength. use examples to try it out
+        # from the other we find out that 
+        # E.g if A is shorter by 2, it's pointer reaches None first and starts iterating on B, by the time B which is two step behind gets to Null and becomes beginning of A, they would meet at same wavelength and get to the intersection together
+        currA = headA
+        currB = headB
+
+        while currA != currB:
+            currA = headB if currA is None else currA.next
+            currB = headA if currB is None else currB.next
+        return currA
+
+
         # Checks the length of the two LL to determine which is longer then makes the pointer start at the same place by moving the longer one by the difference in their length
         # this puts that at the same level as the shorter one, if length are both the same initially, then diff is zero
         # then we iterate through both using two pointers to check the first occurence of which they match
-        # TC: O(m + n + (m - n))
+        # TC: O(m + n + (m - n) + m + n) simplified into O(3m +n) or so, SC: O(N)
+
 
         currA, currB = headA, headB
 
@@ -14,7 +30,6 @@ class Solution:
             return self.findCollision(currA, currB, countA - countB)
         else:
             return self.findCollision(currB, currA, countB - countA)
-        return None
 
 
     def findCollision(self, curr1, curr2, diff):
