b6c2bcc6b5f890255494e746f9ac8af9f9729731
Adeyemi Adedapo <adedapo025@gmail.com>
2025-01-04 18:13:34 +0100

    more solutions and explanations, I don tire

--- LeetCode/Easy/160-Intersection-Of-Two-LLs.py
+++ LeetCode/Easy/160-Intersection-Of-Two-LLs.py
@@ -1,10 +1,47 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # Checks the length of the two LL to determine which is longer then makes the pointer start at the same place by moving the longer one by the difference in their length
+        # this puts that at the same level as the shorter one, if length are both the same initially, then diff is zero
+        # then we iterate through both using two pointers to check the first occurence of which they match
+        # TC: O(m + n + (m - n))
+
+        currA, currB = headA, headB
+
+        countA = self.countLength(currA)
+        countB = self.countLength(currB)
+
+        if countA > countB:
+            return self.findCollision(currA, currB, countA - countB)
+        else:
+            return self.findCollision(currB, currA, countB - countA)
+        return None
+
+
+    def findCollision(self, curr1, curr2, diff):
+        while diff > 0:
+                curr1 = curr1.next
+                diff -= 1
+        while curr1 and curr2:
+            if curr1 == curr2:
+                return curr1
+            curr1 = curr1.next
+            curr2 = curr2.next
+        
+    def countLength(self, curr):
+        count = 0
+        while curr:
+            count += 1
+            curr = curr.next
+        return count
+
+
+
+
         # uses set to note if it has previously been seen
         # goes through one LL storing that the nodes in the set, then traverses through the other one, checking for the first node to have been previoulsy seen
         # TC: O(m + n), SC: O(m)
 
-       hashB = set()
+        hashB = set()
 
         currB = headB
         while currB:
--- LeetCode/Medium/148-Sort-List.py
+++ LeetCode/Medium/148-Sort-List.py
@@ -1,6 +1,37 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # 
+        # uses merge sort to the LL itself
+        if not head or not head.next:
+            return head
+        mid = self.getMid(head)
+        left = head
+        right = mid.next
+        mid.next = None
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left,right)
+
+    def getMid(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+    def merge(self, left,right):
+        dummy = ListNode()
+        tail = dummy
+        while left and right:
+            if left.val <= right.val:
+                tail.next = left
+                left = left.next
+            else:
+                tail.next = right
+                right = right.next
+            tail = tail.next
+        tail.next = left if left else right
+        return dummy.next
 
 
         # used extra space here to convert LL to list, sort then, insert values back
