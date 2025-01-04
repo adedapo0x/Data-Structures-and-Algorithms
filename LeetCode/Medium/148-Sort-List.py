c80e2c182b04d867ce0346b533adadd9b628909a
Adeyemi Adedapo <adedapo025@gmail.com>
2025-01-04 12:57:00 +0100

    added O(N) solution for LC 148

---
+++ LeetCode/Medium/148-Sort-List.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # used extra space here to convert LL to list, sort then, insert values back
+        # TC: O(nlogn), SC: O(N)
+        if not head: return head
+        nodes = []
+        curr = head
+        while curr:
+            nodes.append(curr.val)
+            curr = curr.next
+        nodes.sort()
+        curr = head
+        curr.val = nodes[0]
+        sortedHead = curr
+        i = 1
+        curr = curr.next
+        while curr:
+            curr.val = nodes[i]
+            i += 1
+            curr = curr.next
+        return sortedHead
