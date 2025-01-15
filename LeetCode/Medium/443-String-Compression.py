8842d9db968d4331cd44ca1505e1fe690cb49ec3
Adeyemi Adedapo <adedapo025@gmail.com>
2025-01-15 08:41:25 +0100

    added solution for LC 443

---
+++ LeetCode/Medium/443-String-Compression.py
@@ -0,0 +1,22 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        '''
+        Logic is to use two pointers, the read pointer, traverses through the entire array. The write is to modify the array
+        '''
+        read, write = 0, 0
+        while read < len(chars):
+            start = read # To know the beginning of each repeating consecutive characters
+            # still need to check for read < len(chars) cos of something like [a,a,a,a] so we don't get an out of bounds error
+            while read < len(chars) and chars[read] == chars[start]: 
+                read += 1
+
+            chars[write] = chars[start]
+            write += 1
+
+            # used to add to the array the length of each sequence if greater than one 
+            if read - start > 1:
+                for digit in str(read - start):
+                    chars[write] = digit
+                    write += 1
+        
+        return write  
