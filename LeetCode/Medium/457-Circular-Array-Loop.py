ec018c505b2e52dabc8c8a965e417a3b06caf5b1
Adeyemi Adedapo <adedapo025@gmail.com>
2025-01-17 01:53:26 +0100

    added solution for LC 457

--- LeetCode/Medium/457-Circular-Array-Loop.py
+++ LeetCode/Medium/457-Circular-Array-Loop.py
@@ -1,6 +1,16 @@
 class Solution:
     def circularArrayLoop(self, nums: List[int]) -> bool:
         '''
+        The approach is to use two pointers, normal Floyd's hare tortoise algorithm also known as Fast and slow pointer algorithm, once fast equals slow we can return True
+        we basically loop through the array, since each element denotes by how much index we are moving, also storing the indexes we have encountered in a set in order to prevent redundant checks
+        our currDir and nextDir is to make sure the condition of all the elements in the cycle either being all positive or all negative, say we start with currDir as positive once we get a nextDir as negative, we return -1 knowing that cannot work
+        we do the modulo operator because of the ends of the array, if we move beyond the last element we want to be at the first, if beyond first, we come to the end of the array and keep on counting
+        we check for index not to be equal to nextIndex since that would mean that our cycle contains just one element which is not acceptable
+        we do the fast pointer twice, since for everytime slow moves once, fast is to move twice
+        if we get a -1 at any point we want the inner loop to break and we start checking from the next index and element in the array if it is not already in our set
+        else once fast == slow, we return True
+
+        Yt solution link: https://www.youtube.com/watch?v=cEuJEEbIuJg
         '''
         n = len(nums)
         visitedIndex = set() 
