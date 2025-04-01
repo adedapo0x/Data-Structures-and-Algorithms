73462262781e47e8f7e329eab7fd13b3bfe3e8c7
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-01 17:29:43 +0100

    added iterative implementation of preorder traversal

--- LeetCode/Easy/144-PreOrder-Traversal.py
+++ LeetCode/Easy/144-PreOrder-Traversal.py
@@ -6,6 +6,24 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # Iterative better way of doing it
+        # We use a stack, adding to it right then left, this is because stacks are LIFO, we need left before right and we can get it since it
+        # is the last thing we add to the stack
+        #TC: O(N), SC: O(H) where H is the height, cos the maximum number of nodes that can be in the stack at a time is of same number as height of the tree
+        # which happens when the tree is skewed I think
+        preorder = []
+        if not root:
+            return preorder
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return preorder
+
         # Normal recursive way for doing preorder
         res = []
         def preOrder(root):
