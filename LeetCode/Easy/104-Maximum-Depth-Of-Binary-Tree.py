71da3d1ea0be187a9066324d28d1882eeb0aaa39
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-03 15:56:27 +0100

    added recursive DFS solution for 104

--- LeetCode/Easy/104-Maximum-Depth-Of-Binary-Tree.py
+++ LeetCode/Easy/104-Maximum-Depth-Of-Binary-Tree.py
@@ -6,6 +6,16 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # used recursive DFS, so we find the max depth of both the right and left subtree for each node recursively and add 1. notice that we need to add 1
+        # TC: O(N), all nodes are visited, 
+        # SC: O(h), where h is height, best case for balanced tree is O(logn) where h = logn, worst case is O(n) for degenerate tree where h = n
+        if not root:
+            return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return 1 + max(left, right)
+
+
         # Used BFS here
         if not root:
             return 0
