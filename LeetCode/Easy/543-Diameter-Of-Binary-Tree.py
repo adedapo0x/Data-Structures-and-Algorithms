968f8fcaf3e14f136b30bed7d0ec68edc0b89a39
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-03 20:28:14 +0100

    added optimal solution and notes for diameter of a BT

--- LeetCode/Easy/543-Diameter-Of-Binary-Tree.py
+++ LeetCode/Easy/543-Diameter-Of-Binary-Tree.py
@@ -7,6 +7,28 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         '''
+        Optimal solution, TC: O(N), rather than doing that for each node we encounter, we look for its left and right subtree height then add and compare,
+        here, we do it together, as we get the left and right height of each node that we come across, we calculate the maximum path(diameter) that could be gotten if 
+        that node was the meeting point/curvature of the two subtrees that would make up the path, and at the same time computing the heights to be used.
+
+        Note that for our maxi that we return, we are using a list to store it, cos if we store it as a normal variable, we get error, cause it won't be able to be accessed
+        by the outer method and we would prolly have to do it like the second solution, using the nonlocal or global keyword.
+
+        Took a while to wrap my head around the solution from Striver sef, lmao
+        '''
+        maxi = [0]
+        self.getHeightAndMaxi(root, maxi)
+        return maxi[0]
+    
+    def getHeightAndMaxi(self, node, maxi):
+        if not node:
+            return 0
+        left = self.getHeightAndMaxi(node.left, maxi)
+        right = self.getHeightAndMaxi(node.right, maxi)
+        maxi[0] = max(maxi[0], left + right)
+        return 1 + max(left, right) 
+
+        '''
         Less optimal solution here, uses TC of O(N^2). gets TLE on leetcode
         The approach here is to add the height of the left and right subtree of a node and we get the diameter (longest path), with that node which we are
         taking to find left and right of, as the curvature point of the two nodes mentioned in the question connection path.
@@ -18,9 +40,9 @@ class Solution:
         a bigger connection path (diameter)
         '''
         maxi = 0
-        def findDiameter(node):
-            nonlocal maxi
-            if not node:
+        def findDiameter(node): # 
+            nonlocal maxi # using nolocal here because inside this inner function it is going to treat maxi as another separate variable which we have not previously instantiated
+            if not node:  # note that we could just put maxi in an array, that'll make it a reference that can be accessed inside the inner arrayy
                 return
             l = self.getHeight(node.left)
             r = self.getHeight(node.right)
