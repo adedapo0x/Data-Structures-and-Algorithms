d86c07b80a665df17c12dd66fefe2d3c7209d68e
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-05 14:26:19 +0100

    added notes to my soln of LC 110

--- LeetCode/Easy/110-Balanced-Binary-Tree.py
+++ LeetCode/Easy/110-Balanced-Binary-Tree.py
@@ -9,17 +9,18 @@ class Solution:
         # Came up with this solution myself.
         if not root:
             return True
-        status = [True]
+        status = [True] ## Using this cos of I need it in a reference rather than as a primitive
         self.getHeight(root, status)
         return status[0]
         
 
     def getHeight(self, node, status):
+        # gets the height while checking the status of the tree at the same since it is passed as an argument
         if not node:
             return 0
         left = self.getHeight(node.left, status)
         right = self.getHeight(node.right, status)
-        pre = False if max(left, right) > min(left, right) + 1 else True
+        pre = False if max(left, right) > min(left, right) + 1 else True # checks if the diff between subtree height is more than 1
         status[0] = status[0] and pre
         return 1 + max(left, right)
         
