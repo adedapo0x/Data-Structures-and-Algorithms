ebb62d5cfec17a5e0ca5a97d9d4fa340cd120c22
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-05 14:40:05 +0100

    added bruteforce solution for 110

--- LeetCode/Easy/110-Balanced-Binary-Tree.py
+++ LeetCode/Easy/110-Balanced-Binary-Tree.py
@@ -7,6 +7,7 @@
 class Solution:
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         # Came up with this solution myself.
+        # TC: O(N)
         if not root:
             return True
         status = [True] ## Using this cos of I need it in a reference rather than as a primitive
@@ -23,4 +24,31 @@ class Solution:
         pre = False if max(left, right) > min(left, right) + 1 else True # checks if the diff between subtree height is more than 1
         status[0] = status[0] and pre
         return 1 + max(left, right)
+    
+
+        # Naive / bruteforce solution, 
+        # For each node we first get the left and right subtree height then check if it is balanced, then we move on to the children to get
+        # their own length and this keeps on repeating till we get to the leaf nodes.
+        # TC: O(N^2) since for each node we have to get their left and right subtree which means having to traverse the rest of the tree each time for each node
+        if not root:
+            return True
+        lH = self.getHeight(root.left)
+        rH = self.getHeight(root.right)
+        if abs(lH - rH) > 1:
+            return False
+        left = self.isBalanced(root.left)
+        if not left: return False
+        right = self.isBalanced(root.right)
+        if not right: return False
+
+        return True
+
+
+    def getHeight(self, node):
+        if not node:
+            return 0
+        left = self.getHeight(node.left)
+        right = self.getHeight(node.right)
+        return 1 + max(left, right)
+
         
