9a9f1c4d822186e9b6f67d9aa69396db370b405e
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-06 08:47:07 +0100

    solved search in BST iteratively

---
+++ LeetCode/Easy/700-Search-In-A-BST.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        curr = root
+        while curr and curr.val != val:
+            if val > curr.val:
+                curr = curr.right
+            else:
+                curr = curr.left
+        return curr
+        
