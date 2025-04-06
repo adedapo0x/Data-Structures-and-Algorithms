451c09e8d456902ed181b3579ea365142502950a
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-06 10:51:38 +0100

    added clean DFS solution for Same Tree

--- LeetCode/Easy/100-Same-Tree.py
+++ LeetCode/Easy/100-Same-Tree.py
@@ -6,6 +6,15 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # clean recursive DFS solution, uses like preorder traversal. 
+        # TC: O(p + q)
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        return False # handles condition when one is null, the other is a node, and also when both are nodes but their values are not equal
+
+
         # came up with this solution myself, lmao. not straightforward at all, an AC tho, so a W for me
         # did a postorder traversal on this one 
         if not p and not q:
