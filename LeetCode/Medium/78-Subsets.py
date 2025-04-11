2f659abdfea758e926fafc2c79d04f03d47ad231
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-11 20:15:50 +0100

    added note for why we needed to create copy of arr in Subsets problem

--- LeetCode/Medium/78-Subsets.py
+++ LeetCode/Medium/78-Subsets.py
@@ -9,7 +9,7 @@ class Solution:
     def printSeq(self, arr, nums, indx, res):
         n = len(nums)
         if indx == n:
-            res.append(arr[:])
+            res.append(arr[:]) # doing the arr[:] because we need the actual elements that arr has, if we do just arr, we end up storing just it's reference which would be tampered with by append and pop
             return
         arr.append(nums[indx])
         self.printSeq(arr, nums, indx+1, res)
