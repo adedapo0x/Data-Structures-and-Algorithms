5c04c3ae2ea11fbd713bb20546c2c07efdd47653
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-15 12:38:49 +0100

    added problem title for perfect sum probleM

--- Geeks For Geeks Exercises/Recursion Stuffs/perfect-sum-problem.py
+++ Geeks For Geeks Exercises/Recursion Stuffs/perfect-sum-problem.py
@@ -1,4 +1,5 @@
 '''
+ Perfect Sum Problem
 Given an array arr of non-negative integers and an integer target, the task is to count all subsets of the array whose sum is equal to the given target.
 
 Examples:
