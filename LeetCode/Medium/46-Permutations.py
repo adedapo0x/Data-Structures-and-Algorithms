728c1df7d424239cd3b818fda4ec188404d1002d
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-23 20:33:04 +0100

    added explanatory notes for LC 46

--- LeetCode/Medium/46-Permutations.py
+++ LeetCode/Medium/46-Permutations.py
@@ -1,7 +1,14 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+        '''
+        there is a more optimal approach I think without the extra space, check on striver
+        the approach here is that initially, say for test case [1, 2, 3], it can either start from 1 or from 2 or from 3, so we basically have 
+        n paths the recursion would go, for this example, 3 paths. from there for the one beginning with 1, we can have 1,2 or 1,3 for one starting with 2, we can have
+        2,1 or 2,3. and on and on till we get the temp array same length as num meaning all the elements are in there.
+        The use of a set here is to allow for constant lookup of whether an element is in the array during the recursion rather than doing a O(N) for every check
 
-
+        
+        '''
         res = []
         def computePermute(temp, tempSet):
             if len(temp) == len(nums):
