19ad38795ec21e82f280ca4093d991196dde6100
Adedapo Adeyemi <adedapo025@gmail.com>
2025-04-26 20:30:37 +0100

    added explanatory notes to 17

--- LeetCode/Medium/17-Letter-Combination-Of-A-Phone-Number.py
+++ LeetCode/Medium/17-Letter-Combination-Of-A-Phone-Number.py
@@ -4,7 +4,16 @@ class Solution:
         We initially keep a dictionary to map the numbers to the letters they represent on the phone
         Approach here is to recursively go through each of the characters, starting from the first, and see all combinations that could be made for it, then
         go to the second possible element and repeat the same again and again
-        Implem
+
+        Implementation-wise, we are more of actually going down depthwise to find all the possible combos that can work taking the first character before backtracking 
+        and finding combinations that begin with the second character and so on
+
+        TC: (n * 4^n) we get 4*n because the max amount of letters associated with a number is 4 e.g for 7 and 9. so say worst case scenario, we are given 99, there would have to be 
+        16 combinations, so we have 4 not 3, and also the length of each combination is of length n so the time complexity would be (n * 4^n)
+        SC: o(n) for extra space, but for output list, we get O(n * 4^n)
+
+        Note that we are using a list and the join operation and not just a string because strings in Python are immutable and we would just be creating new strings on every 
+        recursion and that is an expensive O(n) operation 
         '''
 
         mapping =  {
