c3e29c4fb2f690fb5ef715fd590806acfb4a5873
Adedapo Adeyemi <adedapo025@gmail.com>
2025-05-04 10:11:00 +0100

    added problem statement to DFS GFG question

--- Geeks For Geeks Exercises/graphs/dfs-of-a-graph.py
+++ Geeks For Geeks Exercises/graphs/dfs-of-a-graph.py
@@ -1,3 +1,12 @@
+'''
+Given a connected undirected graph containing V vertices represented by a 2-d adjacency list adj[][], 
+where each adj[i] represents the list of vertices connected to vertex i. 
+Perform a Depth First Search (DFS) traversal starting from vertex 0, visiting vertices from left to right as per the given adjacency list,
+and return a list containing the DFS traversal of the graph.
+
+Note: Do traverse in the same order as they are in the given adjacency list.
+'''
+
 class Solution:
     
     #Function to return a list containing the DFS traversal of the graph.
