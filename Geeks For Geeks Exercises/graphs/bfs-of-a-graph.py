6c2cfd2e1c6acfbed38949a6697b3b5934e1e928
Adedapo Adeyemi <adedapo025@gmail.com>
2025-05-04 10:09:20 +0100

    added question to BFS GFG question

--- Geeks For Geeks Exercises/graphs/bfs-of-a-graph.py
+++ Geeks For Geeks Exercises/graphs/bfs-of-a-graph.py
@@ -1,3 +1,12 @@
+'''
+Given a connected undirected graph containing V vertices, represented by a 2-d adjacency list adj[][], where each adj[i] represents the list of vertices connected to vertex i. 
+Perform a Breadth First Search (BFS) traversal starting from vertex 0, visiting vertices from left to right 
+according to the given adjacency list, and return a list containing the BFS traversal of the graph.
+
+Note: Do traverse in the same order as they are in the given adjacency list.
+'''
+
+
 class Solution:
     # Function to return Breadth First Search Traversal of given graph.
     def bfs(self, adj):
