51ff70d9a42ebad85aaa8cdb71961d9e5402943e
Adedapo Adeyemi <adedapo025@gmail.com>
2025-06-14 18:56:55 +0100

    added a similar implementation soln for 127

--- LeetCode/Ooops, Hard/127-Word-Ladder.py
+++ LeetCode/Ooops, Hard/127-Word-Ladder.py
@@ -9,6 +9,7 @@ class Solution:
 
         TC: O(M x N x 26) = O(M x N) where M is the length of the wordList and N is the length of each word in the wordList and since we are 
         checking for 26 letters each time, but the 26 is a constant so no issues there.
+        because of python, the TC would become O(M x N x N x 26) since strings are not dynamic and we have to create a new string everytime with newWord
         SC: O(M) where M is the length of the wordlist that gets converted to a set
         '''
         wordSet = set(wordList)
@@ -26,7 +27,7 @@ class Solution:
             for i in range(len(curr)):
                 for c in "abcdefghijklmnopqrstuvwxyz":
                     if c == curr[i]:
-                        continue
+                        continue 
                     newWord = curr[:i] + c + curr[i+1:]
                     if newWord in wordSet:
                         queue.append((newWord, steps + 1))
@@ -34,7 +35,28 @@ class Solution:
         return 0
 
 
+        # Added another solution for the problem. same approach but here I am using a list here to dynamically change the words
 
+        # queue = collections.deque([(beginWord, 1)])
+        # wordSet = set(wordList)
+        # if endWord not in wordSet:
+        #     return 0
 
+        # wordSet.discard(beginWord)
+        # while queue:
+        #     word, steps = queue.popleft()
+        #     if word == endWord:
+        #         return steps
+        #     wordL = list(word)
+        #     for i in range(len(wordL)):
+        #         originalChar = wordL[i]
+        #         for ch in range(ord("a"), ord("z") + 1):
+        #             wordL[i] = chr(ch)
+        #             mutatedWord = "".join(wordL)
+        #             if mutatedWord in wordSet:
+        #                 queue.append((mutatedWord, steps + 1))
+        #                 wordSet.discard(mutatedWord)
+        #         wordL[i] = originalChar
+        # return 0
 
         
