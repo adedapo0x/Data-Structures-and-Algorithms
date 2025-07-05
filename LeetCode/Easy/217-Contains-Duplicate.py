c7a8cd2d0f1790931bce7f0d546fdfea717fb079
Adedapo Adeyemi <adedapo025@gmail.com>
2025-07-05 15:39:32 +0100

    added another approach to 217

--- LeetCode/Easy/217-Contains-Duplicate.py
+++ LeetCode/Easy/217-Contains-Duplicate.py
@@ -12,3 +12,14 @@ class Solution:
         # a one liner
         # compares the length since a set only allows one occurrence of an element
         return len(nums) != len(set(nums))
+    
+        # TC: O(NlogN), since we sort
+        # when we sort, the duplicates are next to each other so we just check for that
+        # nums.sort()
+        # n = len(nums)
+
+        # for i in range(n - 1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+
+        # return False
