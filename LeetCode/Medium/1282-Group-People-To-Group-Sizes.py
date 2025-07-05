5eda8c02e9a52e194c3f6795802dcd702ba86bac
Adedapo Adeyemi <adedapo025@gmail.com>
2025-07-05 17:10:07 +0100

    optimal solution for 1282 added

--- LeetCode/Medium/1282-Group-People-To-Group-Sizes.py
+++ LeetCode/Medium/1282-Group-People-To-Group-Sizes.py
@@ -1,6 +1,30 @@
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
         '''
+        Approach here is similar to the initial one I came up with down below. Only difference is we do not wait till the dictionary is
+        fully constructed before we start manipulating to append to the ans list
+
+        Here, as we append to the dict values, we check its length, once the length is equal to the group size we want we append that list to our ans,
+        clear it in our hashmap, then clear the list from the hashmap. and we keep on iterating.
+
+        TC: O(N)
+        SC: O(N) due to the hashmap
+        '''
+        hashMap = defaultdict(list)
+        ans = []
+
+        for i in range(len(groupSizes)):
+            size = groupSizes[i]
+            hashMap[size].append(i)
+
+            if len(hashMap[size]) == size:
+                ans.append(hashMap[size])
+                hashMap[size] = []
+
+        return ans
+        
+
+        '''
         Approach here is to run through the input list initially and use a dictionary to store key, a list as values 
         the numbers in the input array (ie group sizes) are the keys and the indexes (ie the people themselves) are the values in a list
 
