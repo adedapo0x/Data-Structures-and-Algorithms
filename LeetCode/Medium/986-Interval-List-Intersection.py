23d9e306c55cd99cbe35e1797b5a6bcd594697eb
Adedapo Adeyemi <adedapo025@gmail.com>
2025-07-12 16:58:20 +0100

    optimal 2 pointers solution added for 986

--- LeetCode/Medium/986-Interval-List-Intersection.py
+++ LeetCode/Medium/986-Interval-List-Intersection.py
@@ -1,5 +1,31 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+
+        if not firstList or not secondList:
+            return []
+
+        ans = []
+
+        p1 = p2 = 0
+
+        while p1 < len(firstList) and p2 < len(secondList):
+            start1, end1 = firstList[p1]
+            start2, end2 = secondList[p2]
+
+            if start2 > end1:
+                p1 += 1
+            elif start1 > end2:
+                p2 += 1
+            else:
+                ans.append([ max(start1, start2), min(end1, end2) ])
+
+                if end1 > end2:
+                    p2 += 1
+                else:
+                    p1 += 1
+
+        return ans
+
         """
         Here, we use two for loops to run through list 1 and list 2. we then compare the start and stop of the arrays to determine the intersection between them.
 
