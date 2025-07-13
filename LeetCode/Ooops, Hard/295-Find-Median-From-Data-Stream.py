4667095f0d482c241640f581efcf0eba9a3dd911
Adedapo Adeyemi <adedapo025@gmail.com>
2025-07-13 01:17:39 +0100

    added notes for naive solution of 295

--- LeetCode/Ooops, Hard/295-Find-Median-From-Data-Stream.py
+++ LeetCode/Ooops, Hard/295-Find-Median-From-Data-Stream.py
@@ -1,4 +1,11 @@
 class MedianFinder:
+    '''
+    Get the median by doing normal calculations, here we use an array as the data structure. So when we initialize the class, we initialize 
+    the values array in it, then to add we simply append and to findMedian we do normal median calculation. 
+
+    This is the naive solution and here, our findMedian function has a time complexity of O(nlogn)
+
+    '''
     def __init__(self):
         self.values = []
     
