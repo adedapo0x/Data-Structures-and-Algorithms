3c2e37f946e27986d10f08a60a74ecc2f567dc63
Adedapo Adeyemi <adedapo025@gmail.com>
2025-07-23 20:12:19 +0100

    added extra notes for optimal soln and included bruteforce soln

--- LeetCode/Medium/347-Top-K-Frequent-Elements.py
+++ LeetCode/Medium/347-Top-K-Frequent-Elements.py
@@ -1,7 +1,13 @@
 class Solution:
     def topKFrequent(self, nums, k: int):
+        # use bucket sort to do it, we use a hashmap to keep occurrence then build a bucket ie an array of arrays and we have each bucket index
+        # as an array to use to store elements that occurs that particular number
+
+        # TC: O(N)
+
         # using the array solution, there's also a heap solution
         # create initial empty hashmap
+
         freq = {}
 
         # instantiate hashmap based on nums array input
@@ -10,6 +16,7 @@ class Solution:
 
         # count array, it's indexes to store frequency
         # group of numbers that occurs same time are in the same array for a particular index(freq) 
+        
         count = [[] for i in range(len(nums) + 1)]
         result = []
         for key, val in freq.items():
@@ -21,3 +28,21 @@ class Solution:
                 result.append(count[i][j])
                 if len(result) == k:
                     return result
+
+
+        count = {}
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+        
+        ans = []
+        for i in range(k):
+            maxi = 0
+            maxKey = 0
+            for key, val in count.items():
+                if val > maxi:
+                    maxi = val
+                    maxKey = key
+
+            ans.append(maxKey)
+            del count[maxKey]
+        return ans
