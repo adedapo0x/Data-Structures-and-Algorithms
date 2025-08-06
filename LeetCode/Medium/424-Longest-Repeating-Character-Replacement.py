df6f249f7970f737e09ff6f925261c77b0b8f56a
Adedapo Adeyemi <adedapo025@gmail.com>
2025-08-06 14:35:33 +0100

    tweaked optimal soln for 424 a bit

--- LeetCode/Medium/424-Longest-Repeating-Character-Replacement.py
+++ LeetCode/Medium/424-Longest-Repeating-Character-Replacement.py
@@ -1,5 +1,6 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        # TC: O(N), SC: O(M) where N is the length of the string and M is the number of unique characters in the string
         count = {}
         l = 0
         res = 0
@@ -10,7 +11,7 @@ class Solution:
             # checks if length (window) of substring minus most appearing character which gives amount of charcters we need to change
             # is bigger than the limit of what we can change given by k
             # if so, the window has to shift so we decrease the value of the character at left pointer in the dictionary and shift the left pointer by 1
-            if (r - l + 1) - max(count.values()) > k:
+            while (r - l + 1) - max(count.values()) > k and l <= r:
                 count[s[l]] -= 1
                 l += 1
             res = max(res, r - l + 1) # then check for max of all the windows we get for the answer
@@ -32,4 +33,7 @@ class Solution:
         #         maxLength = max(maxLength, (r - l + 1))
         #     r += 1
         # return maxLength
+
+
+
 
