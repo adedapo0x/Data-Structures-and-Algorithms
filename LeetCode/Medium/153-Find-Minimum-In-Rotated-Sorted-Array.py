c66b9350558209440fcb71291e8180adfcb2373e
Adedapo Adeyemi <adedapo025@gmail.com>
2025-08-28 14:05:28 +0100

    added more detailed notes for 153

--- LeetCode/Medium/153-Find-Minimum-In-Rotated-Sorted-Array.py
+++ LeetCode/Medium/153-Find-Minimum-In-Rotated-Sorted-Array.py
@@ -1,6 +1,23 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
+        '''
+        Optimal solution here is to use binary search. For a rotated sorted array, there is usually 2 sorted sections of the array, the left sorted part
+        and the right sorted part. This is because rotation of a sorted array simply means taking the smallest element in front of the array and putting it in the back
+        so from this, we can say that there are two sorted parts, and the left sorted part will ALWAYS contain BIGGER numbers than the one in the right sorted part.
 
+        so we get middle index, compare with what we initially have as mini, which is nums[0], could have been any element in the array, just makes more sense to use the first element
+        if smaller, we update the mini variable. then we try to know where to go next, to do that, we have to know if the middle element is in the left sorted part or right sorted part
+        if it is in the left sorted part, we want to go to the right sorted part as the left sorted part would never contain the minimum element
+        if it is in the right sorted part, we try to go left, just to check if there are any lesser elements in the array that are also in the right sorted part.
+
+        we figure out which part we are in like this:
+        if the element at the mid index is greater than or equal to what is in the left index, we are in the left sorted part, this is because for an element to be in the right
+        sorted part, it has to be less than every element in the left sorted part.
+        if element at mid is less than what is in the left, we know the element is in the right sorted part,
+        then we shift our pointers accordingly to find mini
+
+        TC: O(logn), SC: O(1)
+        '''
         l, r = 0, len(nums) - 1
         mini = nums[0]        
         while l <= r:
