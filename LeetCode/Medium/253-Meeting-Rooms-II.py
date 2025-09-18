4e3d3a9c78db854c506ac06353bae3c3555cb8b9
Adedapo Adeyemi <adedapo025@gmail.com>
2025-09-18 14:24:40 +0100

    added heap approach for 253

--- LeetCode/Medium/253-Meeting-Rooms-II.py
+++ LeetCode/Medium/253-Meeting-Rooms-II.py
@@ -1,5 +1,36 @@
+import heapq
+
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        '''
+        The approach here is to sort the intervals based on their start times and then we use a minheap to store the end times as we come across them
+        so initially if the minheap is empty, this is when the loop just starts, we need a new room. We also need a new room if the current start time is lesser than the smallest end time 
+        in the heap, as that means those other meetings are still in session. so we add the end times to the heap to be able to know when they end
+
+        in the else statement, this occurs when the start time is greater or equal to the smallest end time in the heap, here that means the meeting has ended and we are hence free to reuse that
+        room and we replace the end times, (ie we remove/pop the end time of that which has finished and push the end time of the new meeting about to start which is basically what heapq.replace does)
+
+        so at the end we return the length of the minHeap, we can say this is the number of the rooms we need cos if you notice throughout the program we add to the heap when we need a new room
+        and update when we can reuse a room, we do not decrease its length throughout so this would contain the amount of rooms to accommodate all
+
+        good follow up, is why a heap instead of a list, this is because to find the smallest end time, we would need to traverse through the entire list, which is an O(N) operation.
+        this would worsen the entire program TC, heap is better since it gives us O(logN)
+
+        TC: O(NlogN)
+        SC: O(N)        
+        '''
+        sortedIntervals = sorted(intervals, key= lambda x: x[0])
+        minHeap = []
+
+        for interval in sortedIntervals:
+            if not minHeap or interval[0] < minHeap[0]:  # we need a new room
+                heapq.heappush(minHeap, interval[1])
+            else:
+                heapq.heapreplace(minHeap, interval[1]) # we can reuse a room
+
+        return len(minHeap)
+                
+
         """
         The approach here is using two pointers. so we sort both the start time and end time to be able to look at it.
         we can visualize using a number line. since it is sorted, we have the order at which meetings start also the order at which they end, so we know if a new
