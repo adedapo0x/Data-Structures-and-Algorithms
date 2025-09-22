7676fde71595c0ca70d84a92cb0ec8c4a10ea10b
Adedapo Adeyemi <adedapo025@gmail.com>
2025-09-22 21:13:08 +0100

    linear time with O(1) space solution added for 696

--- LeetCode/Easy/696-Count-Binary-Substrings.py
+++ LeetCode/Easy/696-Count-Binary-Substrings.py
@@ -1,6 +1,39 @@
 class Solution:
     def countBinarySubstrings(self, s: str) -> int:
         '''
+        Optimal approach is to note a pattern as to how valid substrings are made.
+        a sequence of a number is like that particular number grouped together, so for example a sequence of 0 can be 0000 or 00, while that of 1 can be 1111111, 1, 11 etc
+        now a boundary is where a sequence change to another sequence, e.g 1111|000|1111, so in this string those places with | are the boundaries
+        
+        now we notice that for a substring to be valid it must contain two sequences with a boundary between them
+        also for that substring we would be able to find how many valid substrings can be derived from that substring with: min(length of first sequence, length of second sequence)
+        e.g if we have a string 000111, this contains 2 sequences and one boundary, the min of the length of the sequence is 3, so 3 valid substrings can be generated from it: 01, 0011, 000111
+        also take 00111, the min of length of sequences is 2, so two substrings: 01, 0011.
+
+        so with this we can get it now, we go through the string starting from index 1, so prevCount keeps count of the previous sequence we saw, initially it is 0
+        currCount is the length of the current sequence we are in, so since we are starting from index 1, we have the currCount set to 1, since we have an element in index 0 that is already been noted as being in the current count
+
+        also need to remember to take min after the loop has ended to add the final valid substrings that would be generated after the last two sequences of the string has been traversed
+
+        TC: O(N), SC: O(1) 
+        '''
+        prevCount = 0
+        currCount = 1
+
+        ans = 0
+
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                currCount += 1
+            else:
+                ans += min(prevCount, currCount)
+                prevCount = currCount
+                currCount = 1
+        ans += min(prevCount, currCount)
+        return ans        
+
+
+        '''
         Bruteforce quadratic time solution, works but gives TLE on Leetcode, too slow
         so what we do is we take each character as the start of a possible substring, and go down s starting from that character to see if we have a valid substring
         
