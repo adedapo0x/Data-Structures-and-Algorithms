9ffc524cf3041eada8ef70ef1ed73516382690f7
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-13 19:22:34 +0100

    iterative approach to 430 added

--- LeetCode/Medium/430-Flatten-MultiLevel-Doubly-LL.py
+++ LeetCode/Medium/430-Flatten-MultiLevel-Doubly-LL.py
@@ -9,6 +9,50 @@ class Node:
 """
 
 class Solution:
+
+    """
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    '''
+    iterative and intuitive solution, just prioritize if there is a child, we store the .next node in a stack and put that node to its child node, then make its .child pointer None
+    the stack is to keep track of all the next nodes that we have previously seen in the correct order as we saw them, so once we get to an end we can pop from the stack and connect that 
+    node to our curr
+
+    TC: O(N), SC: O(N)
+    '''
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return head
+
+        curr = head
+        stack = []
+
+        while curr:
+            if curr.child:
+                if curr.next:
+                    stack.append(curr.next)
+                curr.next = curr.child
+                curr.child.prev = curr
+                curr.child = None
+            if not curr.next and stack:
+                nextNode = stack.pop()
+                curr.next = nextNode
+                nextNode.prev = curr
+            curr = curr.next
+        return head
+    
+
+
+
+
     '''
     We use recursion (DFS) to go through the LL. our recursive function takes prev and curr, two nodes that we want to connect to each other
     we check for a child before we try to connect to next, if child doesn't exists, we return and we then go with next
