9891607a8a59325bddff43b1e64d82f44be2ef6e
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-17 12:28:24 +0100

    solution to 394 added

---
+++ LeetCode/Medium/394-Decode-String.py
@@ -0,0 +1,63 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        '''
+        Here, the approach is to use two stacks. one for letters, one for the numbers, when we come across a letter, we add it to currentString variable, 
+        when we come across digit, add it to the variable k, then when we come across a "[", we append what is in our k and currentString to countStack and stringStack
+        respectively, and reset both k and currentString.
+
+        when we come across a "]", the decoding begins, we pop from both stacks, we take what we popped from countStack and multiply it with whatever our currentString 
+        is, add that to what we popped from stringStack and reassign it back to currentString, and in the end we return currentString, as it would have everything we want
+
+        TC: O(maxK * n) where n is len of s , and maxK is the largest number k in s
+        because we iterate over s and we duplicate strings k times in the loop, so worst case is the largest number of K
+        '''
+        countStack = []
+        stringStack = []
+
+        k = ""
+        currentString = ""
+
+        for c in s:
+            if c.isdigit():
+                k += c
+            elif c == '[':
+                countStack.append(int(k))
+                stringStack.append(currentString)
+                k = ""
+                currentString = ""
+            elif c == "]":
+                decodedString = stringStack.pop()
+                currentK = countStack.pop()
+                currentString = decodedString + currentString * currentK
+            else:
+                currentString += c
+        return currentString
+    
+
+
+        '''
+        More intuitive approach to me, we basically loop through and append the characters we come across to our stack, when we come across "]", it is time to decode
+        so we pop from our stack strategically, we get the string from stack by popping, make sure we arrange them in the correct order, get rid of the "[" that would have
+        preceded by the pop outside the loop, then get the number, store it in k, make sure to get the correct order too as there might be multiple digits, then duplicate the word k times
+        then append to the stack again.
+
+        when we are doing going over s, we can then join all what is in stack together, and that would give us our decoded string
+
+        TC: O(N * maxK * countK)
+        tc and sc are somehow
+        '''
+        stack = []
+
+        for c in s:
+            if c != "]":
+                stack.append(c)
+            else:
+                word = ""
+                while stack[-1] != "[":
+                    word = stack.pop() + word
+                stack.pop()
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k
+                stack.append(word * int(k))
+        return "".join(stack)
