4ac5d00a1507ee8a91ca0bc070d6fa6ccdf22af0
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-20 21:42:23 +0100

    DFS solution for 797 included

--- LeetCode/Medium/797-All-Paths-From-Source-To-Target.py
+++ LeetCode/Medium/797-All-Paths-From-Source-To-Target.py
@@ -1,5 +1,21 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+
+        def backtrack(node, arr):
+            arr.append(node)
+
+            if node == len(graph) - 1:
+                res.append(arr[:])
+                return 
+
+            for neighbour in graph[node]:
+                backtrack(neighbour, arr)
+                arr.pop()
+        
+        res = []
+        backtrack(0, [])
+        return res
+
         # BFS approach, we store the entire path generated each time not just the node
         queue = deque([[0]])
         res = []
