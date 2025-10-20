a0b1665f4f210b3005ab59280c83ca021e371688
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-21 00:06:57 +0100

    notes on DFS approach to 797 included

--- LeetCode/Medium/797-All-Paths-From-Source-To-Target.py
+++ LeetCode/Medium/797-All-Paths-From-Source-To-Target.py
@@ -1,5 +1,15 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        '''
+        DFS approach we take each node then go through its neighbouurs, then when we get to n-1, we append to res, then return
+
+        TC: O(2^N * N) , we get 2^N because each node could have multiple children, and at each juncture, we have to explore each of the multiple paths
+        then for the * N this is because each path has a size of about N nodes, and when we get to target N-1 we do a copy of the elements to res, which is an O(N) operation
+        SC: think this is similar to TC
+
+        note that TC for best case, where we could have like a linear graph, is O(V + E) where we just do a normal DFS one or few choices per node
+        or we could say the TC is O(P * L) for average case, where P is the multiple paths available per node, and L is the length of that path
+        '''
 
         def backtrack(node, arr):
             arr.append(node)
