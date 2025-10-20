897001f9c8981dfc4b161e4167bbacee0f054ca1
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-20 13:35:07 +0100

    added sortedDict approach to 1244

--- LeetCode/Medium/1244-Design-A-Leaderboard.py
+++ LeetCode/Medium/1244-Design-A-Leaderboard.py
@@ -1,5 +1,71 @@
+from sortedcontainers import SortedDict
 class Leaderboard:
+    '''
+    This is an optimization of the getting top operation, in our normal queue solution we would create a queue each time then run through the scores in NlogK time
+    here we optimize the top operation while sacrificing the addScore and reset operations O(1) efficiency
 
+    we make use of a sortedDict, this is not natively supported by Python unlike other languages, so we have to import from sortedContainers. a sorted dict is like a normal dict in 
+    application but here the keys are sorted in ascending order as they are added to the dict. It is efficient with its lookup, insert and remove operation in O(logN) rather than if we wanted to insert then
+    call sort ourselves on the key, that would be O(NlogN) per operation.
+
+    so since the sortedDict is in ascending order, we do the negative trick, so the largest score , say 100, we put it as -100, so it is the first in the dict
+    we map the score to how many people have that score. so to add score, if the player doesn't already exist, we add to both dicts. But if the player exists in our system beforee,
+    we need to get rid of the player's previous score in our sortedDict, so we reduce the number of the people with that prevScore by 1, then put the new updated score, which is 
+    the sum of the prevScore and this new input score, into both our dicts
+    to get top, we basically iterate through the sorted dict, for each score, we keep a count that is increasing by the total number of people that has that score
+    and once we reach K we want to break and return res
+    to reset, we need to update both dicts, if the score of the player is only scored by one person in the sorted dict, we can delete the key which is the score,
+    but if it is scored by more than 1 player, we just decrement the number, then delete the player from our normal dict
+
+    TC: to addScore -> O(logN), to get top -> O(K) since the loop only runs until we have gotten K scores, to reset -> O(logN)
+    SC: O(N)
+
+    '''
+
+    def __init__(self):
+        self.scores = {}
+        self.sortedScores = SortedDict()
+        
+
+    def addScore(self, playerId: int, score: int) -> None:
+        if playerId not in self.scores:
+            self.scores[playerId] = score
+            self.sortedScores[-score] = self.sortedScores.get(-score, 0) + 1
+        else:
+            prevScore = self.scores[playerId]
+            self.sortedScores[-prevScore] -= 1
+
+            newScore = prevScore + score
+
+            self.scores[playerId] = newScore
+            self.sortedScores[-newScore] = self.sortedScores.get(-newScore, 0) + 1 
+
+    def top(self, K: int) -> int:
+        res, count = 0, 0
+        for score, numbers in self.sortedScores.items():
+            for _ in range(numbers):
+                res += (-score) # negate back since we initially stored as -score, so --score == score
+                count += 1
+                if count == K:
+                    break
+
+            if count == K:
+                break
+        return res
+        
+    def reset(self, playerId: int) -> None:
+        score = self.scores[playerId]
+        number = self.sortedScores[-score]
+
+        if number == 1:
+            del self.sortedScores[-score]
+        else:
+            self.sortedScores[-score] -= 1
+        del self.scores[playerId]
+
+
+
+class Leaderboard:
     '''
     Heap solution here, we keep a dictionary of ID to score, to get top, we create a heap of size K, since python gives us min heap by default, 
     as we add to the heap, once the number of elements in the heap is greater than K, we want to do heap.pop, this helps us remove the smallest score in the heap
