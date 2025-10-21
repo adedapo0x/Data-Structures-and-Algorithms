fa5c38867f7939330478a3b60eb828ed2e543b5c
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-21 08:54:44 +0100

    notes to optimal stack approach for 1209

--- LeetCode/Medium/1209-Remove-All-Adjacent-Duplicates-In-String-II.py
+++ LeetCode/Medium/1209-Remove-All-Adjacent-Duplicates-In-String-II.py
@@ -1,6 +1,30 @@
 class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         '''
+        Optimal solution, use a stack to store the characters and the count as we find them, if the top of the stack is equal to the letter we are looking at now,
+        that means they follow each other, then we increment its count, once its count equals k, we pop from the stack and move to the next character
+
+        so what we store in the stack is [char, count] 
+
+        TC: O(N), SC: O(N)
+        '''
+        stack = []
+
+        for c in s:
+            if stack and stack[-1][0] == c:
+                stack[-1][1] += 1
+            else:
+                stack.append([c, 1])
+
+            if stack[-1][1] == k:
+                stack.pop()
+
+        res = ""
+        for char, count in stack:
+            res += (char * count)
+
+        return res
+        '''
         Bruteforce approach, we scan through each time for k adjacent duplicates, once we find one, we change the string, then restart from the beginning
         to look for the next one, we stop only when we complete a pass and the length of the string doesn't change, so length would now be equal to len(s)
 
