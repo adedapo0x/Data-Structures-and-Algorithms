003582799b73a3bef3f4b7f8e8788dd2c9208578
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-22 14:37:16 +0100

    bruteforce addeed to 139

---
+++ LeetCode/Medium/139-Word-Break.py
@@ -0,0 +1,36 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        '''
+        bruteforce approach, here we do recursion, 
+        we start from every index in s and we check if that substring is in wordSet (convert to set for O(1) lookup)
+        if it is, we shift start to the end of the valid index and call the dfs to check for the next substring
+        once we get to the end of s we can return True
+
+        e.g s = "bloomberg", and wordSet = { bloom, berg, bloomber }
+        we check from b up until m, we see that bloom is in our set, so that is covered, we then make a call from b in berg and we get that berg to later and
+        return True as we go up the recursion tree. 
+        say we had an example s = "bloomber", we go bloom, we see that it in set, then we call for b in ber, but there is no ber, so we finish the iteration without calling our recursive check
+        then we return to index 5, then iteration continues checking if bloomb or bloombe, then finally bloomber is valid
+
+        for TC: because at every index we can choose whether to pick our segregation from ther or not, we end up having 2 choices of recursion per index
+        and this grows exponentially and hence not accurate, so TC is O(n.2^n), the extra n is cost of going over all the ends when start from a start
+        and SC is O(N), recursion call stack
+        '''
+         
+        
+
+        wordSet = set(wordDict)
+        n = len(s)
+
+        def check(start):
+            if start == n:
+                return True
+
+            for end in range(start + 1, n + 1):
+                if s[start:end] in wordSet:
+                    if check(end):
+                        return True
+
+            return False
+
+        return check(0)
