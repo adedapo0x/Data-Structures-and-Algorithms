2831e2978a85163347003eb8eeb709fddaea45c4
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-24 18:39:16 +0100

    solution for 1249 added

--- LeetCode/Medium/1249-Minimum-Remove-To-Make-Valid-Parentheses.py
+++ LeetCode/Medium/1249-Minimum-Remove-To-Make-Valid-Parentheses.py
@@ -1,8 +1,50 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         '''
+        Here, we use a count variable to keep track of the count of open and closed brackets we see. when we come across a ( we increment count, and when 
+        we come across a ) we decrement count, while appending to our buildUp. Note that we only decrement count after seeing a ) if count is above 0 ie there is still an unmatched ( that is preceding it
+        if count is not 0, we skip it as including a closing that doesn't have an opening would make our string invalid
+
+        by adding characters and valid parentheses to buildUp list, we would have gotten rid of invalid ), remains invalid openings that is if the openings are more 
+        than the (, we now need to start our iteration from the back and remove the ( we see while decrementing until count becomes zero.
+        we start from the back because it is safer, for example, if we have (()(), we could remove any of the ( and it'll be valid right, but what if we have this, (())(
+        now here we cannot just remove any ( to make count of ( and ) balanced, we need to remove the last (, so in both cases remove from the end is better and that is what we try to do
+        in the second loop while building our filtered.
+
+        note that because we start from the back, the characters would be in reverse order in filtered, and we need to bring them to their right order before
+        we join then return
+
+        TC: O(N) also, SC: O(N)
+        '''
+        count = 0
+        buildUp = []
+
+        for c in s:
+            if c == "(":
+                buildUp.append(c)
+                count += 1
+            elif c == ")":
+                if count > 0:
+                    buildUp.append(c)
+                    count -= 1
+            else:
+                buildUp.append(c)
+        
+        filtered = []
+        for i in range(len(buildUp) - 1, -1 , -1):
+            if buildUp[i] == "(" and count > 0:
+                count -= 1
+                continue
+            filtered.append(buildUp[i])
+
+        filtered = filtered[::-1]
+        return "".join(filtered)
+    
+
+
+        '''
         The approach here is that for a string to be balanced the number of ( and ) must be the same, and there must not be a ) without a ( preceding it
-        so not only count, but also order of the parentheses matter
+        so not only count, but also order of the parentheses matter. we skip the letters in our iterations as they do not matter
 
         Here we keep track of the indexes we want to remove from. as we come across a ( we put in the stack, but if we come across a ) and there is no preceding ( ie there is nothing
         in the stack then we want to get rid of that ), so we put its index in our set, if we come across a ) for which there is something that is in the stack, then we can just pop as the ) would have matched
@@ -13,7 +55,7 @@ class Solution:
         our set, then we then iterate over the string again skipping characters at those indexes we intend to remove, we use a list to keep the characters before joining because if we used a string 
         and was concatenating each time that would be O(N) as strings are immutable and new strings would have been formed each time
 
-        TC: O(2N) = O(N)
+        TC: O(4N) = O(N), union is also O(N) since we are potentially adding all the characters indexes to the set
         SC: O(N)
         
         '''
