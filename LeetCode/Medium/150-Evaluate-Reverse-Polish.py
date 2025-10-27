84cde56a92caaba35f4345233490ebf8757e2bff
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-27 05:34:26 +0100

    brutefoece approach for the 150

--- LeetCode/Medium/150-Evaluate-Reverse-Polish.py
+++ LeetCode/Medium/150-Evaluate-Reverse-Polish.py
@@ -15,3 +15,30 @@ class Solution:
             else:
                 stack.append(int(val))
         return stack[0]
+    
+
+'''
+Bruteforce approach, we keep on modifying the input array whenever we come across a sign, until we have the contents of the array equals 1 element, our final answer
+TC: O(N^2) because of the list slicing and concatenation going on each time
+SC: O(N)
+'''
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        # bruteforce
+
+        while len(tokens) > 1:
+            for i in range(len(tokens)):
+                if tokens[i] in "+-*/":
+                    value1 = int(tokens[i-2])
+                    value2 = int(tokens[i-1])
+                    if tokens[i] == "+":
+                        temp = value1 + value2
+                    elif tokens[i] == "-":
+                        temp = value1 - value2
+                    elif tokens[i] == "*":
+                        temp = value1 * value2
+                    else:       
+                        temp = int(value1 / value2)
+                    tokens = tokens[:i-2] + [str(temp)] + tokens[i+1:]
+                    break
+        return int(tokens[0])
