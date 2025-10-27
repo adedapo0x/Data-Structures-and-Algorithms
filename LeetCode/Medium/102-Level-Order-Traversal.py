1013880cce5c17b40cba414c33112b0880e19687
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-27 11:16:02 +0100

    dfs approach added to 102

--- LeetCode/Medium/102-Level-Order-Traversal.py
+++ LeetCode/Medium/102-Level-Order-Traversal.py
@@ -29,3 +29,30 @@ class Solution:
             res.append(inner)
 
         return res 
+
+
+'''
+Uses DFS approach, a bit less intuitive than the BFS approach, we keep track of the node we are at, and the level we are at, 
+so as we go down the tree we increment depth. depth starts at level 0 meaning the root node
+when we come across a new depth we have not been in before, we know that from length of res being same as depth since we start depth from zero, 
+we append an empty array to res to prepare it for the nodes that come.
+
+TC: O(N), SC: O(K), or worst case (N)
+
+'''
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        def dfsTraversal(node, depth):
+            if not node:
+                return 
+
+            if len(res) == depth:
+                res.append([])
+            
+            res[depth].append(node.val)
+            dfsTraversal(node.left, depth + 1)
+            dfsTraversal(node.right, depth + 1)
+        dfsTraversal(root, 0)
+        return res
