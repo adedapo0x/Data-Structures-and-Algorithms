4b277358a4eb748b6be1e584b2a990b9339db8cf
Adedapo Adeyemi <adedapo025@gmail.com>
2025-10-27 12:08:27 +0100

    add bfs approach for 199

---
+++ LeetCode/Medium/199-Binary-Tree-RightSide-View.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        '''
+        Uses standard BFS, when we are at the last element for a level, that is its rightmost and we can append that into res
+        '''
+        if not root:
+            return []
+        queue = collections.deque([root])
+        res = []
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if i == size - 1:
+                    res.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return res
