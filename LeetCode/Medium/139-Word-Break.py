835df1fffbed8a7fefc88983b5ff76b5c0e654b7
Adedapo Adeyemi <adedapo025@gmail.com>
2025-11-01 15:08:55 +0100

    tabulation DP approach added for 139

--- LeetCode/Medium/139-Word-Break.py
+++ LeetCode/Medium/139-Word-Break.py
@@ -1,6 +1,38 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         '''
+        Approach here is to use tabulation (bottom up approach) DP. here we take a list that contains boolean values for each index of s if a
+        segmentation can be made at that index. since s[a:b] doesn't include b, when we give dp[] its size, it has to be len(s) + 1, since to cover the end of the string
+        at len(s) - 1, our index would be at len(s) and arrays initializing would have done 0 to n - 1
+
+        we set dp[0] to True as this is the trivial case in our bottom up, an empty string can always be segregated. we then start looping from index 1, ie covering only the 
+        first character at index 0 using variable i, the j is to use traverse from 0 up until i to check if there is a possible segregation between 0 and j-1, and j to i - 1
+        here we rely on previously solved overlapping subproblems, hence why it is DP. for each i, we go using j from 0 to i, to check, since our initial j = 0, dp[j] is always true, 
+        we are looking for a segmentation from 0 up until that index. once we find one, that dp[j] = True and that word after j to i-1 is in wordSet, that means thats two words from j to i - 1
+
+        as we go once we find a possible segmentation, we mark that index as True in our dp[], break, then continue the loop for i, 
+        we know that once we reach the end of the string and we mark it as true, that means there is a valid segmentation path from the start of the string to the end of the string
+        so we return dp[len(string)] since that covers up until the end of the string at index len(s) - 1
+
+        TC: O(N^2), SC: O(N)
+        '''
+        wordSet = set(wordDict)
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+            
+        return dp[len(s)]
+
+
+
+
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        '''
         Here, we use memoization, that is the only difference with what is in the bruteforce, we save ourselves the cost of having to recompute whether a segmentation
         can be made while starting from an index by storing the answer in a dict the first time the result for that index returns, whether True or False
 
@@ -31,7 +63,7 @@ class Solution:
             return False
         return check(0)
         '''
-        bruteforce approach, here we do recursion, 
+        bruteforce approach, gets TLE on Leetcode, here we do recursion, 
         we start from every index in s and we check if that substring is in wordSet (convert to set for O(1) lookup)
         if it is, we shift start to the end of the valid index and call the dfs to check for the next substring
         once we get to the end of s we can return True
